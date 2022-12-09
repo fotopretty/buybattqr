48608061e91ef2d8b04d8ff88b64ccc683902fa1
fotopretty <fotopretty@gmail.com>
2022-12-09 19:59:53 +0700

    Add existing file

--- index.js
+++ index.js
@@ -6,13 +6,6 @@ const request = require("request");
 const port = process.env.PORT || 3000;
 const TOKEN = process.env.LINE_ACCESS_TOKEN;
 
-const LINE_MESSAGING_API = 'https://api.line.me/v2/bot/message';
-const LINE_HEADER = {
-  'Content-Type': 'application/json',
-  'Authorization': 'Bearer 6GhvnaaBtEWipStWBjaC/Z8/PehPsK1SCHuN2QS+V7wM2hZEVxdJMDVIqjhBVENj1Q1SV1/BtVGE7JUBDY19ihpR1Sg6h+HpEKYuHtP7Ux3Qg2Gcx+yyt0H3H9ZlhYtQB1QlDokyXF9bQJh9uStMjwdB04t89/1O/w1cDnyilFU'
-};
-
-
 // Reply with two static messages
 
 app.use(bodyParser.urlencoded({ extended: false }));
