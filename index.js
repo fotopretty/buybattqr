c601c770a519fcc8f50a8103d7012c5149512196
fotopretty <fotopretty@gmail.com>
2022-12-12 09:17:31 +0700

    Starting Node project

--- index.js
+++ index.js
@@ -6,13 +6,10 @@ const request = require("request");
 const port = process.env.PORT || 3000;
 const TOKEN = process.env.LINE_ACCESS_TOKEN;
 
-
-
 // Reply with two static messages
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 
 /* app.get("/", (req, res) => {
   res.sendStatus(200);
@@ -21,7 +18,7 @@ app.use(bodyParser.json());
 app.post("/webhook", (req, res) => {
   res.send("HTTP POST request sent to the webhook URL!");
 
-    let reply_token = req.body.events[0].replyToken;
+  let reply_token = req.body.events[0].replyToken;
   reply(reply_token);
   res.sendStatus(200);
 });
@@ -30,14 +27,14 @@ app.listen(port, () => {
   console.log(`Example app listening at ${port}`);
 });
 
-function reply(reply_token,msg) {
+function reply(reply_token, msg) {
   let headers = {
     "Content-Type": "application/json",
     Authorization:
       "Bearer 6GhvnaaBtEWipStWBjaC/Z8/PehPsK1SCHuN2QS+V7wM2hZEVxdJMDVIqjhBVENj1Q1SV1/BtVGE7JUBDY19ihpR1Sg6h+HpEKYuHtP7Ux3Qg2Gcx+yyt0H3H9ZlhYtQB1QlDokyXF9bQJh9uStMjwdB04t89/1O/w1cDnyilFU=",
   };
 
-    let body = JSON.stringify({
+  let body = JSON.stringify({
     replyToken: reply_token,
     messages: [
       {
@@ -47,8 +44,8 @@ function reply(reply_token,msg) {
       {
         type: "text",
         text: "How are you?",
-      }
-    ]
+      },
+    ],
   });
 
   request.post(
